45579f51a02c686c98ec0107fd2db1eb49163338
MotoSaku <gracegloria63@gmail.com>
2022-01-20 14:54:29 +0900

    商品カテゴリーのActiveHash実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,8 @@ class ItemsController < ApplicationController
   # def delete
   # end
 
+  private
+
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end

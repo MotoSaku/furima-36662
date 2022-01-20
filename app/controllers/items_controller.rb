2283c82dc696c485b4ebf20e318388884efd67e0
MotoSaku <gracegloria63@gmail.com>
2022-01-20 10:15:04 +0900

    ビューの実装、画像ファイルActive Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,19 @@
 class ItemsController < ApplicationController
 
-def index
-end
+  def index
+  end
 
-def new
-end
+  def new
+  end
 
-# def edit
-# end
+  # def edit
+  # end
 
-# def delete
-# end
+  # def delete
+  # end
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
 end

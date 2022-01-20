f778d9476a62fe7647d0d4d29151141e1c17245f
MotoSaku <gracegloria63@gmail.com>
2022-01-20 15:11:10 +0900

    発送料、発送元地域、配送日数のプルダウン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category_id
   belongs_to :status_id
+  belongs_to :charge_id
   belongs_to :city_id
+  belongs_to :delivery_id
 
   #空の投稿を保存できないようにする
   #validates :title, :text, presence: true
@@ -14,4 +16,6 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :city_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" }  
 end
